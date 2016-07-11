3f31f009ff133440521c6792ff99eeb42a3f07f8
Amal <amaldevv@outlook.com>
2016-07-11 09:54:46 +0530

    Added console logging features

--- core/LoggingSample/Controllers/ErrorController.cs
+++ core/LoggingSample/Controllers/ErrorController.cs
@@ -1,10 +1,18 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace LoggingSample.Controllers
 {
     public class ErrorController : Controller
     {
+        ILogger<HomeController> _logger;
+
+        public ErrorController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
         public IActionResult Index()
         {
             return View();
@@ -13,6 +21,9 @@ namespace LoggingSample.Controllers
         [Route("/Error/{ErrorCode}")]
         public IActionResult Error(int ErrorCode)
         {
+            var moreInfo = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            _logger.LogInformation("Error occurred with Status Code : {StatusCode}, Original Path : {Original Path}",
+                ErrorCode, moreInfo.OriginalPath);
             return View(ErrorCode);
         }
     }
