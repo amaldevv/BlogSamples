b92bd3bad47165e36926a9ca90b58a2b3366da90
Amal <amaldevv@outlook.com>
2016-07-19 09:22:20 +0530

    Removed unused references

--- core/LoggingSample/Startup.cs
+++ core/LoggingSample/Startup.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
-using Microsoft.Extensions.PlatformAbstractions;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
